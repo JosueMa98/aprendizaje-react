1f95d832b53bb089976972b73a251ed42320046d
Maldonado Arana Victor Josue <L20171583@culiacan.tecnm.mx>
2025-06-22 14:08:49 -0600

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function retornarNumeroAleatorio() {
+  return Math.floor(Math.random() * 100);
+}
+
+function retornarIndiceDeMasaCorporal(peso, altura) {
+  const indiceMasa = peso / (altura * altura);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Su indice de masa es= {indiceMasa}</h1>
+  );
+
+}
+
+function App() {
+  const nombre = "josue";
+  const edad = 26;
+
+  const objetoPerro = {
+    nombre: "Pucca",
+    edad: "11 años"
+  };
+
+  const arregloPaginas = ["http://google.com", "http://facebook.com", "http://youtube.com"];
+
+  return (
+    <div>
+      <h1>Retornando numero aleatorio desde funcion: {retornarNumeroAleatorio()}</h1>
+      <h2>Usando nombre de constante: {nombre}</h2>
+      <h3>Usando la edad de la constante por el numero de la funcion aleatoria: {retornarNumeroAleatorio()*edad}</h3>
+      <h4>Usando el nombre del objeto perro</h4>
+      <p>Nombre: {objetoPerro.nombre}, Edad: {objetoPerro.edad}</p>
+      <hr/>
+      <a href={arregloPaginas[0]}>Acceso a google </a>
+      <br/>
+      <a href={arregloPaginas[2]}>Acceso a youtube </a>
+      <br/>
+      <h5>Retornando su indice de masa en base a su peso: 80kg y su altura: 170cm</h5>
+      <p>Resultado= {retornarIndiceDeMasaCorporal(80, 1.7)}</p>
     </div>
   );
 }
